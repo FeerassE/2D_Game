e42b954f8bcb66535003401f4b64b675068947fc
Taimoor <taimoor.azfar@gmail.com>
2019-03-13 22:44:16 -0400

    cube game added

---
+++ cube game/tutorial 1/Assets/PlayerCollision.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+    public PlayerMovement movement;
+	// Use this for initialization
+	void OnCollisionEnter(Collision collisioninfo)
+    {
+        if (collisioninfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+}
